9bb4336d49dd28e39e2ea840e557ab1f34dd643c
Mark Aziz <markaziz93@gmail.com>
2019-06-18 22:32:58 +1200

    fix ecosystem and edited package.json

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
   apps: [{
     name: 'spotify-frontend',
     script: "npm",
-    args : "start",
+    args : "run start:production',",
     env: {
       NODE_ENV: "development",
     },
