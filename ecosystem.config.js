e804ec2a38718e532dea979a693894a28985f3f0
Mark Aziz <markaziz93@gmail.com>
2019-06-18 21:54:15 +1200

    set up pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  apps: [{
+    name: 'spotify-frontend',
+    script: './index.js',
+    env: {
+      NODE_ENV: "development",
+    },
+    env_production: {
+      NODE_ENV: "production",
+    }
+    
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-13-239-6-208.ap-southeast-2.compute.amazonaws.com',
+      key: '~/.ssh/spotify-app-node.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:m-aziz/spotify-frontend.git',
+      path: '/home/ubuntu/spotify-frontend',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
