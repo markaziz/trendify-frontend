8835426113c62c8f3729039f7d370756af579465
Mark Aziz <markaziz93@gmail.com>
2019-06-29 02:02:38 +1200

    routing bug

--- src/components/MySongs/MySongs.js
+++ src/components/MySongs/MySongs.js
@@ -2,6 +2,13 @@ import React, { useEffect } from 'react';
 import GuessPopularity from '../GuessPopularity/GuessPopularity';
 
 export default function MySongs(props) {
+
+  useEffect(() => {
+    if (!localStorage.getItem('accessToken')) {
+      window.location.href = '/';
+    }
+  })
+
   const location = {
     state: {
       accessToken: localStorage.getItem('accessToken'),
