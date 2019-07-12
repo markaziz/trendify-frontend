25d30de500102f8431185417cf2e4f74b7f800df
Mark Aziz <markaziz93@gmail.com>
2019-07-13 00:09:15 +1200

    fix mobile styling

--- src/components/MUISlider/MUISlider.js
+++ src/components/MUISlider/MUISlider.js
@@ -1,19 +1,28 @@
 import React from 'react';
 import { Slider } from 'material-ui-slider';
+import { makeStyles } from '@material-ui/core/styles';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    minHeight: '8rem'
+  },
+}));
 export default function MUISlider(props) {
   const { value, onChange, defaultValue } = props;
+  const classes = useStyles();
 
   return (
-    <React.Fragment>
+    <div className={classes.root}>
       <h1 id="slider">{value}</h1>
       <Slider
+      
         aria-labelledby="slider"
         defaultValue={defaultValue}
         value={value || defaultValue}
         onChange={onChange}
         getAriaValueText={value}
+        classes={{ root: "sliderRoot" }}
       />
-    </React.Fragment>
+    </div>
   );
 }
